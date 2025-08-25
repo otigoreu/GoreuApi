9933d3258ab85c9779737c6a9dd633744d560ea1
otigoreu <eponce@regionucayali.gob.pe>
2025-08-25 09:55:55 -0500

    RolResponseDto con nivel

--- Goreu.DTO/Response/RolResponseDto.cs
+++ Goreu.DTO/Response/RolResponseDto.cs
@@ -9,6 +9,7 @@ namespace Goreu.DtoResponse
     public class RolResponseDto
     {
         public string Id { get; set; } = default!;
+        public char Nivel { get; set; } = default!;
         public string Name { get; set; } = default!;
         public string NormalizedName { get; set; } = default!;
         public bool Estado { get; set; } = default!;
