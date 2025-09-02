a27a93b6dc96359fd818a878bde21561cf3aa941
PieroLleli <pllerena@regionucayali.gob.pe>
2025-09-02 12:47:37 -0500

    refactor: Limpiar y verificar rolresponsedto

--- Goreu.DTO/Response/RolResponseDto.cs
+++ Goreu.DTO/Response/RolResponseDto.cs
@@ -7,8 +7,6 @@
         public string Name { get; set; } = default!;
         public string NormalizedName { get; set; } = default!;
         public bool Estado { get; set; } = default!;
-
-        public char Nivel { get; set; }
         public int idEntidadAplicacion { get; set; }
     }
 }
