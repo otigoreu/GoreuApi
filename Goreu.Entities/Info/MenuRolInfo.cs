9b40372889cb70eec87dd680c75d7378447972ab
PieroLleli <pllerena@regionucayali.gob.pe>
2026-02-02 10:03:44 -0500

    fix: Se agrego un campo mas, menu padre

--- Goreu.Entities/Info/MenuRolInfo.cs
+++ Goreu.Entities/Info/MenuRolInfo.cs
@@ -6,6 +6,7 @@
         public bool Operacion { get; set; }
         public bool Consulta { get; set; }
         public int IdMenu { get; set; }
+        public int? IdMenuPadre { get; set; }
         public string DescripcionMenu { get; set; }
         public string IconoMenu { get; set; } 
         public string IdRol { get; set; }
